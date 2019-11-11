603b176f35b2b3c9c5e9661b17a89ea0a8284569
chrzyki <christoph@functor-argument.net>
2019-11-11 09:40:22 +0100

    Code formatting

--- lexibank_beidasinitic.py
+++ lexibank_beidasinitic.py
@@ -10,23 +10,27 @@ from clldutils.misc import slug
 import lingpy
 from lingpy.sequence.sound_classes import syllabify
 
+
 @attr.s
 class CustomConcept(Concept):
-    Chinese_Gloss = attr.ib(default=None, metadata={'name_in_source': 'number'})
-    Number = attr.ib(default=None, metadata={'name_in_source': 'number'})
+    Chinese_Gloss = attr.ib(default=None, metadata={"name_in_source": "number"})
+    Number = attr.ib(default=None, metadata={"name_in_source": "number"})
+
 
 @attr.s
 class CustomLanguage(Language):
     Latitude = attr.ib(default=None)
     Longitude = attr.ib(default=None)
     ChineseName = attr.ib(default=None)
-    SubGroup = attr.ib(default='Sinitic')
-    Family = attr.ib(default='Sino-Tibetan')
+    SubGroup = attr.ib(default="Sinitic")
+    Family = attr.ib(default="Sino-Tibetan")
     DialectGroup = attr.ib(default=None)
+
 
 @attr.s
 class CustomLexeme(Lexeme):
     Benzi = attr.ib(default=None)
+
 
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
@@ -36,37 +40,40 @@ class Dataset(BaseDataset):
     lexeme_class = CustomLexeme
 
     def cmd_download(self, **kw):
-        self.raw.write("sources.bib", getEvoBibAsBibtex("Cihui", **kw))
+        self.raw_dir.write("sources.bib", getEvoBibAsBibtex("Cihui", **kw))
 
     def cmd_makecldf(self, args):
         # load data as wordlists, as we need to bring the already segmented
         # entries in line with clts
-        wl = lingpy.Wordlist(self.raw_dir.joinpath("words.tsv").as_posix(),
-                conf=self.raw_dir.joinpath("wordlist.rc").as_posix())
-        wl.add_entries('new_segments', 'segments', lambda x: syllabify(
-            self.tokenizer(
-                '', '^'+''.join(x)+'$',
-                column="IPA"
-                ),
-            cldf=True))
+        wl = lingpy.Wordlist(
+            self.raw_dir.joinpath("words.tsv").as_posix(),
+            conf=self.raw_dir.joinpath("wordlist.rc").as_posix(),
+        )
+        wl.add_entries(
+            "new_segments",
+            "segments",
+            lambda x: syllabify(
+                self.tokenizer("", "^" + "".join(x) + "$", column="IPA"), cldf=True
+            ),
+        )
 
         args.writer.add_sources(*self.raw_dir.read_bib())
-        
+
         # TODO: add concepts with `add_concepts`
         concept_lookup = {}
         for concept in self.conceptlists[0].concepts.values():
-            idx = concept.id.split('-')[-1]+'_'+slug(concept.gloss)
+            idx = concept.id.split("-")[-1] + "_" + slug(concept.gloss)
             args.writer.add_concept(
-                    ID=idx,
-                    Name=concept.gloss,
-                    Chinese_Gloss=concept.attributes['chinese'],
-                    Number=concept.number,
-                    Concepticon_ID=concept.concepticon_id,
-                    Concepticon_Gloss=concept.concepticon_gloss
-                    )
+                ID=idx,
+                Name=concept.gloss,
+                Chinese_Gloss=concept.attributes["chinese"],
+                Number=concept.number,
+                Concepticon_ID=concept.concepticon_id,
+                Concepticon_Gloss=concept.concepticon_gloss,
+            )
             concept_lookup[concept.number] = idx
         # use the new lookup_factory here
-        language_lookup = args.writer.add_languages(lookup_factory='Name')
+        language_lookup = args.writer.add_languages(lookup_factory="Name")
 
         for k in progressbar(wl, desc="wl-to-cldf", total=len(wl)):
             if wl[k, "value"]:
@@ -77,5 +84,5 @@ class Dataset(BaseDataset):
                     Form=wl[k, "form"],
                     Segments=wl[k, "new_segments"],
                     Source="Cihui",
-                    Benzi=wl[k, 'benzi']
+                    Benzi=wl[k, "benzi"],
                 )
